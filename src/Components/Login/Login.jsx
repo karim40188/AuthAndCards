5b1348a9fa79c6ff8a47148d80dc37fc21303fbc
karim mohamed <karim40189@gmail.com>
2024-09-08 22:24:15 +0200

    some edit

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,5 +1,6 @@
 import "@mui/material";
 import loginImg from "../../assets/login.png";
+
 import {
   Box,
   Checkbox,
@@ -15,19 +16,19 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 function Login() {
-  const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  let navigate = useNavigate();
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
 
-  const theme = createTheme({
+  const newTheme = createTheme({
     components: {
       MuiTextField: {
         styleOverrides: {
           root: {
             "& .MuiOutlinedInput-root": {
               borderRadius: "15px",
-              width: "100%",
-              height: "60px",
+              width: "100%", // Make TextField full width in smaller screens
+              height: "80px",
             },
           },
         },
@@ -35,47 +36,38 @@ function Login() {
     },
   });
 
-  const handleSubmit = (event) => {
+  function handleSubmit(event) {
     event.preventDefault();
     console.log({ email, password });
     localStorage.setItem("user", email);
-    navigate("/showcards");
-  };
+  }
 
   return (
-    <ThemeProvider theme={theme}>
-      <Box
-        sx={{
-          display: "flex",
-          height: "100vh",
-          alignItems: "center",
-          justifyContent: "center",
-          padding: "15px",
-          backgroundColor: "#f7f7f7",
-        }}
-      >
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={6} sx={{ padding: "15px" }}>
+    <ThemeProvider theme={newTheme}>
+      <Box sx={{ padding: { xs: '10px', md: '20px' }, margin: '0 auto' }}>
+        <Grid container >
+          <Grid item xs={12} md={6} sx={{ display: 'flex', flexDirection: 'column', alignItems: { xs: 'center', md: 'flex-start' }, textAlign: { xs: 'center', md: 'left' } }}>
             <Typography
-              sx={{ fontSize: { xs: "30px", sm: "40px", md: "50px" }, fontWeight: "700", color: "#0F1F32" }}
+              sx={{ fontSize: { xs: '30px', md: '50px' }, fontWeight: '700', color: "#0F1F32" }}
             >
               Logo
             </Typography>
+
             <Box>
               <Typography
                 sx={{
-                  fontSize: { xs: "30px", sm: "40px", md: "50px" },
-                  fontWeight: "400",
-                  color: '#D76320',
+                  fontSize: { xs: '30px', md: '50px' },
+                  fontWeight: '400',
+                  color: "#D76320",
                 }}
               >
                 Hello,
               </Typography>
               <Typography
                 sx={{
-                  fontSize: { xs: "40px", sm: "50px", md: "60px" },
-                  fontWeight: "700",
-                  color: '#D76320',
+                  fontSize: { xs: '40px', md: '60px' },
+                  fontWeight: '700',
+                  color: "#D76320",
                   height: "80px",
                 }}
               >
@@ -84,57 +76,61 @@ function Login() {
               <Box
                 component="form"
                 onSubmit={handleSubmit}
-                sx={{ display: "flex", flexDirection: "column" }}
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: { xs: 'center', md: 'flex-start' },
+                }}
               >
                 <TextField
                   id="email"
-                  label="Email"
+                  label="email"
                   placeholder="E-Mail"
                   onChange={(e) => setEmail(e.target.value)}
                   value={email}
-                  sx={{ marginBottom: "15px" }}
+                  sx={{ marginBlock: "10px", width: { xs: '90%', sm: '80%', md: '415px' } }}
                 />
                 <TextField
                   id="password"
-                  label="Password"
-                  placeholder="Password"
+                  label="password"
+                  placeholder="password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
-                  sx={{ marginBottom: "15px" }}
+                  sx={{ marginBlock: "10px", width: { xs: '90%', sm: '80%', md: '415px' } }}
                 />
                 <FormControlLabel
                   control={<Checkbox sx={{ color: "#B5B5B5" }} />}
                   label="Remember me"
-                  sx={{ color: "#B5B5B5" }}
+                  sx={{ color: "#B5B5B5", marginTop: "10px" }}
                 />
-                <Box sx={{ display: "flex", flexDirection: { xs: "column", sm: "row" }, gap: "10px" }}>
+                <Box sx={{ display: "flex", flexDirection: { xs: 'column', sm: 'row' }, gap: "10px", alignItems: { xs: 'center', md: 'flex-start' } }}>
                   <Button
                     sx={{
-                      width: "100%",
-                      maxWidth: "175px",
+                      width: { xs: '100%', sm: '175px' },
                       height: "51px",
                       borderRadius: "10px",
-                      backgroundColor: '#D76320',
-                      color: '#fff',
+                      backgroundColor: "#D76320",
+                      color: "#fff",
                       textTransform: "capitalize",
-                      fontSize: "16px",
+                      fontSize: "20px",
                       fontWeight: "700",
                     }}
                     type="submit"
+                    onClick={() => navigate("/showcards")}
                   >
                     Login
                   </Button>
                   <Button
                     sx={{
-                      width: "100%",
-                      maxWidth: "175px",
+                      width: { xs: '100%', sm: '175px' },
                       height: "51px",
                       borderRadius: "10px",
-                      color: '#D76320',
+                      color: "#D76320",
                       textTransform: "capitalize",
-                      fontSize: "16px",
+                      fontSize: "20px",
                       fontWeight: "700",
                       border: "1px solid #D76320",
+                      marginLeft: { xs: '0', sm: '10px' },
                     }}
                   >
                     Register
@@ -143,9 +139,13 @@ function Login() {
               </Box>
             </Box>
           </Grid>
-          <Grid item xs={12} md={6} sx={{ display: { xs: "none", md: "block" } }}>
+          <Grid item xs={12} md={6}>
             <Box
-              sx={{ width: "100%", height: "auto" }}
+              sx={{
+                width: "100%",
+                height: "auto",
+                display: { xs: "none", md: "block" }, // Show image only in medium and larger screens
+              }}
               component="img"
               src={loginImg}
               alt="Login Illustration"
