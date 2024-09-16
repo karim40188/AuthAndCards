02605b0523917589cfcc877882ade2536ddb2483
karim mohamed <karim40189@gmail.com>
2024-09-16 04:05:06 +0200

    some edit

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -5,18 +5,17 @@ import { useTheme } from "@emotion/react";
 import Navbar from "../Navbar/Navbar";
 import { SideBarToggleContext } from "../Context/SideBarToggleContext";
 import { useContext } from "react";
-import { useTranslation } from 'react-i18next'; 
+import { useTranslation } from "react-i18next";
 
 function Layout() {
   let { isSidebarOpen } = useContext(SideBarToggleContext);
   let theme = useTheme();
-  const { i18n } = useTranslation(); 
+  const { i18n } = useTranslation();
 
-
-  const isRtl = i18n.language === 'ar'; 
+  const isRtl = i18n.language === "ar";
 
   return (
-    <Box sx={{ display: "flex", direction: isRtl ? 'rtl' : 'ltr' }}>
+    <Box sx={{ display: "flex", direction: isRtl ? "rtl" : "ltr" }}>
       {/* SideBar */}
       <SideBar />
 
@@ -24,8 +23,8 @@ function Layout() {
       <Box
         sx={{
           backgroundColor: theme.palette.background.default,
-          width: '100%',
-          minHeight: '100vh',
+          width: "100%",
+          minHeight: "100vh",
         }}
       >
         {/* Navbar */}
@@ -33,8 +32,16 @@ function Layout() {
         <Box
           sx={{
             marginTop: { xs: "210px", md: "230px" },
-            marginRight: isRtl ? (isSidebarOpen ? { xs: "300px", md: "400px" } : "70px") : undefined,
-            marginLeft: !isRtl ? (isSidebarOpen ? { xs: "350px", md: "400px" } : {xs:'70px',md:'100px'}) : undefined,
+            marginRight: isRtl
+              ? isSidebarOpen
+                ? { xs: "300px", md: "400px" }
+                : "0px"
+              : undefined,
+            marginLeft: !isRtl
+              ? isSidebarOpen
+                ? { xs: "350px", md: "400px" }
+                : ""
+              : undefined,
             transition: "all 0.3s ease",
           }}
         >
